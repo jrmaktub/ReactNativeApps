dec9e43c7ef0e4edbb1600d32a628962db70d81c
jramirez09 <jramirez12@luc.edu>
2022-02-01 16:03:06 -0600

    fixed error. Added Login and SignUp

--- NewsApp/src/navigation/AuthStack.js
+++ NewsApp/src/navigation/AuthStack.js
@@ -1,48 +1,50 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import { View, Text } from 'react-native'
+import {createStackNavigator} from '@react-navigation/stack';
+import Tabs from './Tabs';
+import SplashScreen from '@Screen/SplashScreen';
+import Login from '@Screen/Login';
+import Register from '@Screen/Register';
+import Onboarding from '@Screen/Onboarding';
+import NewsDetails from '@Screen/NewsDetails';
+import CategoryList from '@Screen/CategoryList';
+import About from '@Screen/About';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-import {createStackNavigator} from '@react-navigation/stack'
-import Tab from './Tabs'
-import SplashScreen from '@Screen/SplashScreen'
-import Login from '@Screen/Login'
-import Register from '@Screen/Register'
-import Onboarding from '@Screen/Onboarding'
-import NewsDetails from '../screens/NewsDetails'
 
-import CategoryList from '../screens/CategoryList'
-import About from '../screens/About'
-
-
-const AuthStack = () =>{
-
-    const Stack = createStackNavigator()
-
-    
+const AuthStack = ({...props}) => {
+    const {isOnboardingDisabled} = props;
+    const Stack = createStackNavigator();
 
     return (
         <Stack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+            initialRouteName={isOnboardingDisabled ? 'Tab' : 'Onboarding' }>
 
-        screenOptions={{
-            headerShown: false
-        }}
-        
-        initialRoutName="Tab"
-
-        >
-            
-            <Stack.Screen name="Tab" component={Tab}/>
-            <Stack.Screen name="Splash" component={SplashScreen}/>
-            
-
-            <Stack.Screen name="Login" component={Login}/>
-            <Stack.Screen name="Register" component={Register}/>
-            <Stack.Screen name="Onboarding" component={Onboarding}/>
-            <Stack.Screen name="NewsDetails" component={NewsDetails}/>
-            <Stack.Screen name="CategoryList" component={CategoryList}/>
-            <Stack.Screen name="About" component={About}/>
-
+            <Stack.Screen name="Splash" component={SplashScreen} />
+            <Stack.Screen name="Tab" component={Tabs} />
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Register" component={Register} />
+            <Stack.Screen name="Onboarding" component={Onboarding} />
+            <Stack.Screen name="NewsDetails" component={NewsDetails} />
+            <Stack.Screen name="CategoryList" component={CategoryList} />
+            <Stack.Screen name="About" component={About} />
         </Stack.Navigator>
     )
 }
 
-export default AuthStack
+
+AuthStack.propTypes = {
+    isOnboardingDisabled: PropTypes.bool.isRequired
+}
+
+const mapStateToProps = (state) => {
+    return {
+        isOnboardingDisabled: state.auth.isOnboardingDisabled
+    }
+}
+
+export default connect(mapStateToProps)(AuthStack);
